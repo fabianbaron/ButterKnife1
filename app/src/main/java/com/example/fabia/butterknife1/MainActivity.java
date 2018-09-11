299ce683cf7c1dccfa877083e2d89de68bfd1f4f
Fabian <fabian.baron@gmail.com>
2018-09-10 19:36:24 -0500

    Initial branch commit

--- app/src/main/java/com/example/fabia/butterknife1/MainActivity.java
+++ app/src/main/java/com/example/fabia/butterknife1/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.fabia.butterknife1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.myTextView)
+    TextView myTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
